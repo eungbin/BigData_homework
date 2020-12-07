b4e91542eac96533f0b75c35f676c240045131a9
kim <programmer97@naver.com>
2020-12-07 16:04:35 +0900

    future_data_1

--- homework/covid-sido.py
+++ homework/covid-sido.py
@@ -14,6 +14,7 @@ matplotlib.rc('font', family=font_name)
 matplotlib.rcParams['axes.unicode_minus'] = False
 
 data_covid_sido = pd.read_excel('./Covid19SidoInfState.xlsx')
+model_dir = './future_models'
 
 dic_sido = {"인천": "IC", "강원": "KW", "경기": "KK", "경남": "KN", "경북": "KB", "광주": "GJ", "대구": "DG",
             "대전": "DG", "부산": "BS", "서울": "SU", "세종": "SJ", "울산": "US", "전남": "JN", "전북": "JB",
@@ -60,10 +61,10 @@ def model_create_and_run(name, cnt): #모델 정의, 데이터 전처리, 훈련
     model.fit(x_train, y_train,
               validation_data=(x_test, y_test),
               batch_size=10,
-              epochs=20)
+              epochs=50)
 
     # 가중치파일 저장
-    model.save('./models/{0}.h5'.format(name))
+    model.save('{0}/{1}.h5'.format(model_dir, name))
 
 def model_predict(name, cnt, hangeul_name):
     origin = cnt[0]
@@ -78,12 +79,13 @@ def model_predict(name, cnt, hangeul_name):
     x_test = result[:]
     x_test = np.reshape(x_test, (1, x_test.shape[0], 1))
 
-    model = load_model('./models/{0}.h5'.format(name))
+    model = load_model('{0}/{1}.h5'.format(model_dir, name))
     pred = model.predict(x_test)
     pred_result = (pred+1)*origin
     print("{0}'s result : ".format(hangeul_name), pred_result)
     return pred_result
 
+# 시/도별로 데이터를 나눈 뒤 각각 배열에 저장하여 모델훈련
 for list in dic_sido:   # 키값 하나씩 불러옴
     globals()['data_{}'.format(dic_sido[list])] = data_covid_sido.groupby('gubun').get_group(list)
     globals()['data_{}'.format(dic_sido[list])].drop(['seq', 'gubunCn', 'gubunEn', 'deathCnt', 'incDec', 'isolClearCnt', 'qurRate',
@@ -96,9 +98,36 @@ for list in dic_sido:   # 키값 하나씩 불러옴
     # model_create_and_run(dic_sido[list], defCnt)
     # print("{0}의 코로나 누적확진자 예측모델 훈련완료".format(list))
 
+# 훈련된 모델을 이용하여 실제 값을 예측
+# for list in dic_sido:
+#     df_cnt_50 = globals()['data_{}'.format(dic_sido[list])]['defCnt'].tail(50)
+#     cnt_50 = []
+#     for cnt in df_cnt_50:
+#         cnt_50.append(cnt)
+#     model_predict(dic_sido[list], cnt_50, list)
+
+
+# 더욱 먼 미래의 데이터 예측
 for list in dic_sido:
+    globals()['list_{}'.format(dic_sido[list])] = []
+    defCnt = []
+    for cnt in globals()['data_{}'.format(dic_sido[list])]['defCnt']:
+        defCnt.append(int(cnt))
     df_cnt_50 = globals()['data_{}'.format(dic_sido[list])]['defCnt'].tail(50)
     cnt_50 = []
     for cnt in df_cnt_50:
         cnt_50.append(cnt)
-    model_predict(dic_sido[list], cnt_50, list)
+    # 향후 12일간의 누적확진자수 예측
+    for i in range(12):
+        model_create_and_run(dic_sido[list], defCnt)
+        pred_result = model_predict(dic_sido[list], cnt_50, list)
+        defCnt.append(pred_result)
+        # list_result.append(pred_result)
+        globals()['list_{}'.format(dic_sido[list])].append(pred_result)
+        del cnt_50[0]
+        cnt_50.append(pred_result)
+        print("{0}의 향후 {1}일 데이터 예측 완료".format(list, i)
+    print("{0}의 12일치 데이터 예측 완료".format(list))
+
+print(list_IC)
+print(list_SU)
